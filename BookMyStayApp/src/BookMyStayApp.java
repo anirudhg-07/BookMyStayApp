c97ee0678f15ce6b0a4e68749031927113e3b3cc
Anirudh G <anirudhg1605@gmail.com>
2026-03-24 09:38:57 +0530

    Booking Request (First-Come-First-Served)

--- BookMyStayApp/src/BookMyStayApp.java
+++ BookMyStayApp/src/BookMyStayApp.java
@@ -1,91 +1,66 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.Queue;
 
-class RoomInventory {
-    private Map<String, Integer> roomAvailability;
+class Reservation {
+    private String guestName;
+    private String roomType;
 
-    public RoomInventory() {
-        roomAvailability = new HashMap<>();
-        initializeInventory();
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
     }
 
-    private void initializeInventory() {
-        roomAvailability.put("Single", 5);
-        roomAvailability.put("Double", 3);
-        roomAvailability.put("Suite", 2);
+    public String getGuestName() {
+        return guestName;
     }
 
-    public Map<String, Integer> getRoomAvailability() {
-        return roomAvailability;
-    }
-}
-
-class Room {
-    private String type;
-    private int beds;
-    private int size;
-    private double price;
-
-    public Room(String type, int beds, int size, double price) {
-        this.type = type;
-        this.beds = beds;
-        this.size = size;
-        this.price = price;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public int getBeds() {
-        return beds;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public double getPrice() {
-        return price;
+    public String getRoomType() {
+        return roomType;
     }
 }
 
-class RoomService {
-    private RoomInventory inventory;
+class BookingRequestQueue {
+    private Queue<Reservation> requestQueue;
 
-    public RoomService(RoomInventory inventory) {
-        this.inventory = inventory;
+    public BookingRequestQueue() {
+        requestQueue = new LinkedList<>();
     }
 
-    public void showAvailability() {
-        Room single = new Room("Single", 1, 250, 1500.0);
-        Room doubleRoom = new Room("Double", 2, 400, 2500.0);
-        Room suite = new Room("Suite", 3, 750, 5000.0);
-
-        display(single);
-        display(doubleRoom);
-        display(suite);
+    public void addRequest(Reservation reservation) {
+        requestQueue.offer(reservation);
     }
 
-    private void display(Room room) {
-        Map<String, Integer> availabilityMap = inventory.getRoomAvailability();
-        int available = availabilityMap.get(room.getType());
+    public Reservation getNextRequest() {
+        return requestQueue.poll();
+    }
 
-        if (available > 0) {
-            System.out.println(room.getType() + " Room:");
-            System.out.println("Beds: " + room.getBeds());
-            System.out.println("Size: " + room.getSize() + " sqft");
-            System.out.println("Price per night: " + room.getPrice());
-            System.out.println("Available: " + available);
-            System.out.println();
-        }
+    public boolean hasPendingRequests() {
+        return !requestQueue.isEmpty();
     }
 }
 
 public class BookMyStayApp {
     public static void main(String[] args) {
-        RoomInventory inventory = new RoomInventory();
-        RoomService service = new RoomService(inventory);
-        service.showAvailability();
+        System.out.println("Booking Request Queue");
+
+        BookingRequestQueue bookingQueue = new BookingRequestQueue();
+
+        Reservation r1 = new Reservation("Abhi", "Single");
+        Reservation r2 = new Reservation("Subha", "Double");
+        Reservation r3 = new Reservation("Vanmathi", "Suite");
+
+        bookingQueue.addRequest(r1);
+        bookingQueue.addRequest(r2);
+        bookingQueue.addRequest(r3);
+
+        while (bookingQueue.hasPendingRequests()) {
+            Reservation next = bookingQueue.getNextRequest();
+            System.out.println(
+                    "Processing booking for Guest: " +
+                            next.getGuestName() +
+                            ", Room Type: " +
+                            next.getRoomType()
+            );
+        }
     }
 }
